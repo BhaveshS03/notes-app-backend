73c7ecc8715f6aeafd0e2af6c864072542bb40ad
BhaveshS03 <bhaveshsonar003@gmail.com>
2025-10-12 22:37:13 +0530

    remove useless

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -15,18 +15,3 @@ export const getFilePaths = (roomId: string, persistenceDir: string) => ({
   markdown: `${persistenceDir}/${roomId}-markdown.txt`,
   meta: `${persistenceDir}/${roomId}-meta.json`
 });
-
-  passport.use(
-    new GoogleStrategy(
-      {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback",
-      },
-      (accessToken, refreshToken, profile, done) => {
-        return done(null, profile);
-      }
-    )
-  );
-  passport.serializeUser((user, done) => done(null, user));
-  passport.deserializeUser((user, done) => done(null, user));
