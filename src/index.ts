a5a988b7e66f2cd9c325035d6deadbdc3c126e5a
BhaveshS03 <bhaveshsonar003@gmail.com>
2025-12-06 12:38:48 +0530

    update cors

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,6 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://gentle-elf-19ba24.netlify.app",
       "https://main.d220red9g3z692.amplifyapp.com",
       "http://localhost:8080"
     ],
